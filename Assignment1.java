725fc7574b42188177fa61f158c657683690ee13
PranaliWankhede <49021488+PranaliWankhede@users.noreply.github.com>
2019-04-04 19:55:12 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,19 @@
+class calculation
+{
+	int a,b;
+	
+	void add()
+	{
+		a=10; b=20;
+		int c = a+b;
+		System.out.println("Addition = "+c);
+	}
+	public static void main(String args[])
+	{
+		calculation obj= new calculation();
+		obj.add();
+		
+		
+	}
+
+}
